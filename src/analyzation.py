7d9b27e580c1fa3dc53cb4efd605efee4be53dbc
DNSL\danil <danilpz05@gmail.com>
2025-04-23 22:49:50 -0400

    Have the functionality for both the analyzation.py and log_parser.py files nearly completely finished.

--- src/analyzation.py
+++ src/analyzation.py
@@ -1,2 +1,10 @@
 # Use the logs collected by log_parser to generate a useful json file that will be used in
 # the visualizer.py file
+from collections import Counter
+import pandas as pd
+
+def aggregate_ips(ip_list, out_path="data/attacks.csv"):
+    count = Counter(ip_list)
+    df = pd.DataFrame(count.items(), columns=["IP", "Attempts"])
+    df = df.sort_values(by="Attempts", ascending=False)
+    df.to_csv(out_path, index=False)
--- src/log_parser.py
+++ src/log_parser.py
@@ -1,1 +1,27 @@
 # Read the log specified in the call and log IPs by count using the detections
+import re
+patterns = {
+    'failed_login': r'Failed password.*from (\d+\.\d+\.\d+\.\d+)',
+    'invalid_user': r'Invalid user (\w+)',
+    'sudo_fail': r'sudo:.*authentication failure',
+    'accepted_login':    r'Accepted password for .* from (\d+\.\d+\.\d+\.\d+)',
+    'ssh_disconnect': r'Connection closed by (\d+\.\d+\.\d+\.\d+)',
+    'su_failed': r'su: .*incorrect password',
+    'pam_failure': r'pam_unix\(sshd:auth\): authentication failure; .* rhost=(\d+\.\d+\.\d+\.\d+)'
+}
+
+def parse_auth_log(path):
+    events = []
+
+    with open(path, 'r') as f:
+        for line in f:
+            for event_type, pattern in patterns.items():
+                match = re.search(pattern, line)
+                if match:
+                    ip = match.group(1) if match.groups() else "N/A"
+                    events.append({
+                        'ip': ip,
+                        'event': event_type,
+                        'raw': line.strip()
+                    })
+    return events
